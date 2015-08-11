72d195018a2210d2f2630f218bdd2c8e1f966533
Mary Lee <leejmary@gmail.com>
2015-08-11 01:07:52 -0500

    vicolo initial commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+(function () {
+
+	var mobileWidth = 720;
+	var desktopWidth = 1200;
+
+	//BXSLIDER
+	var windowWidth = $(window).width();
+	var slider = false;
+	var bxSlider;
+
+	$(document).ready(function () {
+
+		var toggleSliderView = function() {
+			if (windowWidth <= mobileWidth && slider === false) {
+				bxSlider = $('.bxslider').bxSlider({
+					mode: 'horizontal',
+					auto: false,
+					controls: false,
+					adaptiveHeight: true,
+					infiniteLoop: false
+				});
+				slider = true;
+			} else if (windowWidth > mobileWidth && slider === true) {
+				bxSlider.destroySlider();
+				slider = false;
+			}
+		};
+
+		toggleSliderView();
+
+		$(window).resize(function() {
+			windowWidth = $(document).width();
+			toggleSliderView();
+		});
+
+		//SCROLL TO INTERNAL LINKS
+		$('a[href^="#"]').click(function(e){
+			e.preventDefault();
+			
+			var target = this.hash;
+			var $target = $(target);
+
+			$('html, body').stop().animate({'scrollTop': $target.offset().top}, 900, 'swing', function () {
+				window.location.hash = target;
+			});
+		});
+
+		//SCROLL TO TOP OF PAGE
+		$(window).scroll(function () {
+			var bodyScrollTop = $('body').scrollTop();
+			if (bodyScrollTop > 500 && $(window).width() >= desktopWidth) {
+				$('.scroll-to-top').fadeIn('fast');
+			} else {
+				$('.scroll-to-top').fadeOut('fast');
+			}
+		});
+
+		$('.scroll-to-top').click(function () {
+			$('body').animate({ scrollTop: 0 });
+		});
+
+		//CUSTOM TOURS TOGGLE SLIDE
+		$('#tours .button').click(function () {
+			$('#custom-tour').slideToggle();
+			$(this).find('i').toggleClass('fa-chevron-right fa-chevron-down');
+		});
+	});
+}());
